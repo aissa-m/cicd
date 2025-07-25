99da968a9f1ef3156e1f04e37a2ef86951692c8e
aissa-m <issamaymoutt17@gmail.com>
2025-07-25 10:16:40 +0200

    pruebas ci/cd

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+// Puerto
+const PORT = process.env.PORT || 3000;
+
+// Ruta de prueba
+app.get('/', (req, res) => {
+  res.send('✅ ¡Hola desde tu app Node.js con CI/CD!');
+});
+
+// Iniciar servidor
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+});
+
+module.exports = app; // para los tests si los usas
